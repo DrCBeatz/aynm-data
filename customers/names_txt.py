2f067d12f1531aa63bce682ca86edf884589f56d
Bryne Carruthers <63172116+DrCBeatz@users.noreply.github.com>
2024-11-08 15:40:56 -0500

    Add names_txt.py to customers directory

---
+++ customers/names_txt.py
@@ -0,0 +1,11 @@
+names_txt = """NAME: [sample_name_1]
+EMAIL: [sample_email_1]
+PHONE: [sample_phone_#_1]
+
+NAME: [sample_name_2]
+EMAIL: [sample_email_2]
+PHONE: [sample_phone_#_2]
+
+NAME: [sample_name_3]
+EMAIL: [sample_email_3]
+PHONE: [sample_phone_#_3]"""
